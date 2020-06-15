e0634053f1e54cacd2f6d8996b976109c47901f1
Web82-Thom <thom.orta@gmail.com>
2020-06-15 16:20:12 +0200

    ajout des fichiers pour la page d'accueil

---
+++ view/home.php
@@ -0,0 +1,11 @@
+<?php $title = "Acceuil"; ?>
+
+
+<?php ob_start() ?>
+<?php include('menu.php'); ?>
+
+
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
---
+++ view/template.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title><?= $title ?></title>
+        <link rel="stylesheet" href="public\css\style.css" type="text/css" />
+        <?php include("head.php"); ?>
+    </head>
+        
+    <body>
+        <div id="blocPage">
+            <?= $content ?>
+        </div> 
+    </body>
+</html>
