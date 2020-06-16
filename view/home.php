cb8abc649ffe04310f4c33d75d93a336796158c6
Web82-Thom <thom.orta@gmail.com>
2020-06-16 10:21:28 +0200

    modifier home.php et style.css pour: insertion de la photo autheur

--- view/home.php
+++ view/home.php
@@ -3,7 +3,14 @@
 
 <?php ob_start(); ?>
 <?php include('menu.php'); ?>
+<div id="contentPresentation">
+    <div id="photoAuthor">
+        <div id="contentPhoto">
+            <img src="public\images\photoAuthor.jpg" alt="JeanForteroche" class="photoAuthor" />
+        </div>
+    </div>
 
+</div>
 
 <?php $content = ob_get_clean(); ?>
 
