3771f45bc91ca29bd2bb9b4479b78c0b25030257
Web82-Thom <thom.orta@gmail.com>
2020-06-17 08:13:36 +0200

    ajout de lien au menu

--- view/menu.php
+++ view/menu.php
@@ -2,7 +2,7 @@
 <h1>Jean Forteroche</h1>
 <h2>"Billet simple pour l'Alaska"</h2>
 <ul id="menuNavigation">
-    <li> <a href="#"></a> Présentation</li>
-    <li> <a href="#"></a> Billet simple</li>
-    <li> <a href="#"></a> Contact</li>
+    <li> <a href="index.php"> Présentation</a></li>
+    <li> <a href="#">Billet simple</a></li>
+    <li> <a href="#">Contact</a></li>
 </ul>
