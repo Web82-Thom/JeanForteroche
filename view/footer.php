1094c008636fa5040edb3e2cbc090c66c2a3991d
Web82-Thom <thom.orta@gmail.com>
2020-06-19 09:41:57 +0200

    formating des fichiers

---
+++ view/footer.php
@@ -0,0 +1,8 @@
+<!--FOOTER-->
+<footer>
+<ul id="menuFooter">
+    <li> <a href="#">Présentation</a></li>
+    <li> <a href="#">Billet simple</a></li>
+    <li> <a href="#">Contact</a></li>
+</ul>
+</footer>
---
+++ view/menu.php
@@ -0,0 +1,8 @@
+
+<h1>Jean Forteroche</h1>
+<h2>"Billet simple pour l'Alaska"</h2>
+<ul id="menuNavigation">
+    <li> <a href="index.php"> Présentation</a></li>
+    <li> <a href="#">Billet simple</a></li>
+    <li> <a href="#">Contact</a></li>
+</ul>
