18626ca76a8a55d3f3ffdf33d12f71d9a967655c
Web82-Thom <thom.orta@gmail.com>
2020-06-19 09:19:45 +0200

    formating du fichier

--- views/singleTicket.php
+++ views/singleTicket.php
@@ -2,9 +2,13 @@
 
 
 <?php ob_start(); ?>
+<?php foreach($posts as $post); ?>
 
 <h2>Mes billets en ligne</h2>
 <p>Mes derniers billets</p>
+<h3><?= $post->title(); ?></h3> <!--getters dans post.php*/ -->
+<time><?= $post->cretion_date(); ?><time>
+
 
 <?php $content = ob_get_clean(); ?>
 
