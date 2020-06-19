fcf39b1c961ad61cd941f758cf1b115dcf1306ad
Web82-Thom <thom.orta@gmail.com>
2020-06-19 09:49:52 +0200

    resolution des dossiers en pagaillle!!! view remplace views

--- view/footer.php
+++ view/footer.php
@@ -1,8 +1,8 @@
 <!--FOOTER-->
 <footer>
 <ul id="menuFooter">
-    <li> <a href="#">Présentation</a></li>
-    <li> <a href="#">Billet simple</a></li>
+    <li> <a href="index.php">Présentation</a></li>
+    <li> <a href="views\singleTicket.php">Billet simple</a></li>
     <li> <a href="#">Contact</a></li>
 </ul>
 </footer>
--- views/footer.php
+++
@@ -1,8 +0,0 @@
-<!--FOOTER-->
-<footer>
-<ul id="menuFooter">
-    <li> <a href="index.php">Présentation</a></li>
-    <li> <a href="views\singleTicket.php">Billet simple</a></li>
-    <li> <a href="#">Contact</a></li>
-</ul>
-</footer>
