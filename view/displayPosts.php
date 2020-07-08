1e0f58ed050db385c22b025e9aa34c5d4220721e
Web82-Thom <thom.orta@gmail.com>
2020-07-08 17:20:27 +0200

    formating

--- view/displayPosts.php
+++ view/displayPosts.php
@@ -4,7 +4,7 @@
 <h2 id="titleListTicket">liste des Billets</h2>
 <?php foreach ($posts as $post) { ?>
 <div class="contentPosts">
-<h3 class="titleTicket"><a href="index.php?action=post&amp;id=<?= $post->id(); ?>"><?= htmlspecialchars($post->title());?></a> publié le <?= $post->creation_date();?></h3>
+<h3 class="titleTicket"><a href="index.php?objet=post&amp;id=<?= $post->id(); ?>"><?= htmlspecialchars($post->title());?></a> publié le <?= $post->creation_date();?></h3>
     <div id="post">
         <p class="postText"><?= $post->content();?></p>
         <p class="linkComment"><em><a class="link" href="../view/comment.php">Commentaires</a></em></p>
@@ -15,9 +15,3 @@
 <?php $content = ob_get_clean(); ?>
 
 <?php require_once('template.php');
-
-/**
- * 
- * lien pour comment.php ressemblera a 
- * <p class="linkComment"><em><a href="index.php?action=post&amp;id=<?= $post['id'] ?>">Commentaires</a></em></p>
- */
