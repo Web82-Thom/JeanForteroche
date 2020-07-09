ae0d4610de66a79ea51a65665024968aa7a53655
Web82-Thom <thom.orta@gmail.com>
2020-07-10 00:51:35 +0200

    modification de la condition et redirection apres l'ajout du post

--- Controller/PostController.php
+++ Controller/PostController.php
@@ -34,9 +34,9 @@ class PostController
             $postId = $this->_postManager->add($_POST['title'], $_POST['content']);
             echo 'chapitre bien poster';
             //si postId est un nombre alors redirection sur displayPost de $postId
-            if (is_int($postId)) {
-            header('Location: index.php?objet=post&id=' . $postId);
-            }
+            //if (is_int($postId)) {
+            header('Location: index.php?action=post&objet=admin');
+            //}
             throw new Exception('Impossible d\'ajouter le commentaire !');
         } else {
             require_once('../view/formAddPost.php');
