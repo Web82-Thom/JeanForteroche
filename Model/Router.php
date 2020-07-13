c6a103ef8d69ab10ceb5f7d3cbd919f27229e604
Web82-Thom <thom.orta@gmail.com>
2020-07-13 17:57:39 +0200

    mise en palce de la suppression d'un post

--- Model/Router.php
+++ Model/Router.php
@@ -18,25 +18,32 @@ class Router extends PostController
             if (isset($_GET['objet']) || isset($_GET['action'])) {
                 //$url = explode('/', filter_var($_GET['url'], FILTER_SANITIZE_URL));
                 //AFFICHAGE DES BILLETS
-                if ($_GET['objet'] === 'post') {
+                if (isset($_GET['objet']) && ($_GET['objet'] === 'post')) {
                     $postController = new PostController();
                     //AFFICHAGE D'1 POST
                     if (isset($_GET['id'])) {
                         $postController->display($_GET['id']);
                     // AJOUT D'UN POST
-                    } elseif (isset($_GET['action'])) {
+                    } elseif (isset($_GET['action']) && ($_GET['action'] === 'add')) {
+                    var_dump('action');
+                    $postController->add();
+                    // SUPPRIMER UN POST
+                    } elseif (isset($_GET['action']) && ($_GET['action'] === 'delete')) {
                         var_dump($_GET['action']);
-                        $postController->add();
+                        // arnaque sur le (isset($_GET['id']))
+                        $postId = $_GET['postId'];
+                        $postController = new PostController();
+                        $postController->delete($postId);
                     // AFFICHAGE DE TOUS LES POST
                     } else {
-                        $postController->displayPosts();
-                    }   
+                    $postController->displayPosts();
+                    }
                 //REDIRECTION SUR L'ADMIN
-                } elseif ($_GET['objet'] === 'admin') {
+                } elseif (isset($_GET['objet']) && ($_GET['objet'] === 'admin')) {
                     $adminController = new AdminController();
                     $adminController->display();
                 //REDIRECTION SUR L'INDEX.PHP
-                } elseif ($_GET['objet'] === 'home') {
+                } elseif (isset($_GET['objet']) && ($_GET['objet'] === 'home')) {
                     header ("Location: index.php");
                 }
             //SI L'UTILISATEUR FAIT RIEN "PAGE D'ACCUEIL"
