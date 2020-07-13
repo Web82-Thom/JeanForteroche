06979755cd22d7e43f66426f2a4e47ed69d76f0b
Web82-Thom <thom.orta@gmail.com>
2020-07-13 18:08:17 +0200

    ajout affichage des posts

--- Controller/AdminController.php
+++ Controller/AdminController.php
@@ -1,11 +1,14 @@
 <?php
 
 namespace Controller;
+use Model\PostManager;
 
 class AdminController
 {
     public function display()
-    {   
+    {   //affichage des posts sur admin.php
+        $this->_postManager = new PostManager();
+        $posts = $this->_postManager->getPosts();
         require_once('../view/admin.php');
     }
 }
