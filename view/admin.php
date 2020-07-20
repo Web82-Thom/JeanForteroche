ef412fad9ece31ddafad33aaa7ce4b1314ec4c90
Web82-Thom <thom.orta@gmail.com>
2020-07-20 23:34:05 +0200

    formating et mise en place des liens

--- view/admin.php
+++ view/admin.php
@@ -2,29 +2,24 @@
 
 <?php ob_start(); ?>
 
-<h3 id="titleAdmin">Administrateur</h3>
-<p>Bonjour Mr Forteroche</p>
-<h4>Creation d'un chapitre</h4>
-<a href="index.php?objet=post&action=add">Ajouter un poste</a> <br>
-<h4>Modifier ou suprimer un post</h4>
-<?php foreach ($posts as $post) { ?>
-    
-<table id="table">
-               
-    <tr>
-        <td><?= (htmlspecialchars($post->title()));?></td>
-        <td><a href="#">Modifier</a></td>
-        <td><a href="index.php?objet=post&action=delete&postId=<?=$post->id();?>">Supprimer</a></td>
-        
-    </tr>
-  
-</table>   
-
-<?php } ?>
-
+    <div class="adminPage">
+        <h3 id="titleAdmin">Administrateur</h3>
+        <p class="welcomeAdmin">Bonjour Mr Forteroche, cette page vous est consacrée pour gerer votre site.</p>
+        <h4>Creation d'un chapitre</h4>
+            <ul>
+                <li><a class="adminLink" href="index.php?objet=post&action=add">Ajouter un chapitre</a></li><br>
+            </ul>
+        <h4>Modifier ou suprimer un chapitre</h4>
+        <?php foreach ($posts as $post) { ?>
+            <ul>
+                <li><?= (htmlspecialchars($post->getTitle()));?><br/>
+                <a class="adminLink" href="index.php?objet=post&action=update&id=<?= $post->getId(); ?>">Modifier</a><br/>
+                <a class="adminLink" href="index.php?objet=post&action=delete&id=<?= $post->getId(); ?>">Supprimer</a></li>
+            </ul>
+        <?php } ?>
+    </div>
 <?php
 $content = ob_get_clean(); 
 
-require_once('template.php'); 
-
+require_once('template.php');
 ?>
