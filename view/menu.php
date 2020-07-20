ac381caf862cdfc092276d95e2cf03bcd1a0ee5c
Web82-Thom <thom.orta@gmail.com>
2020-07-20 23:40:36 +0200

    Foramating

--- view/menu.php
+++ view/menu.php
@@ -1,14 +1,8 @@
-<?php
-
-?>
-
 <h1>Jean Forteroche</h1>
 <h2>"Billet simple pour l'Alaska"</h2>
-<ul id="menuNavigation">
-    <li> <a href="index.php?objet=home">Présentation</a></li>
-    <li> <a href="index.php?objet=post">Liste des Billets</a></li>
-    <li> <a href="#">Contact</a></li>
-    <li> <a href="index.php?objet=admin">Admin</a></li>
-</ul>
-
-<?php
+    <ul id="menuNavigation">
+        <li> <a href="index.php?objet=home">Présentation</a></li>
+        <li> <a href="index.php?objet=post">Liste des Billets</a></li>
+        <li> <a href="#">Contact</a></li>
+        <li> <a href="index.php?objet=admin">Admin</a></li>
+    </ul>
