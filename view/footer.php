c01e0afb20ad98729b7afa05bfd69b357e9ba8e6
Web82-Thom <thom.orta@gmail.com>
2020-07-20 23:38:01 +0200

    Ajout des liens

--- view/footer.php
+++ view/footer.php
@@ -1,8 +1,9 @@
 <!--FOOTER-->
 <footer>
 <ul id="menuFooter">
-    <li> <a href="index.php?action=home"> Présentation</a></li>
-    <li> <a href="index.php?action=post">Liste des Billets</a></li>
+    <li> <a href="index.php?objet=home"> Présentation</a></li>
+    <li> <a href="index.php?objet=post">Liste des Billets</a></li>
     <li> <a href="#">Contact</a></li>
+    <li> <a href="index.php?objet=admin">Admin</a></li>
 </ul>
 </footer>
