2d771a2b709cd9cac47a55358c60c62ed848208a
Web82-Thom <thom.orta@gmail.com>
2020-07-20 23:26:33 +0200

    méthode updated et delete modification et ajout des parametres

--- Model/PostManager.php
+++ Model/PostManager.php
@@ -26,18 +26,20 @@ class PostManager extends Database
 
         return $post;
     }
+
     //METHODE POUR RECUPERER 1 POSTS
     public function getPost($postId)
     {
-        $req = $this->getDataBase()->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = ?');
+        $req = $this->getDataBase()->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date FROM posts WHERE id = ?');
         $req->execute(array($postId));
 
         return $this->hydrate($req->fetch(PDO::FETCH_ASSOC));
     }
+    
     //METHODE POUR RECUPERER LES POSTS
     public function getPosts()
     {
-        $req = $this->getDataBase()->prepare(' SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ');
+        $req = $this->getDataBase()->prepare(' SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date FROM posts ');
         $req->execute();
         $posts = [];
         while($data = $req->fetch(PDO::FETCH_ASSOC)) {
@@ -47,23 +49,30 @@ class PostManager extends Database
         
         return $posts;
     }
+
     // METHODE AJOUT D'UN POSTE
     public function add($title, $content)
     {
         $req = $this->getDataBase()->prepare('INSERT INTO posts (title, content, creation_date) VALUES(?, ?, NOW())');
+        $req->execute(array($title, $content));
 
-        return $req->execute(array($title, $content));
-        
-        // RENVOYER LE LastInsertId()
-        $lastId = $this->getDataBase()->lastInsertId();
-        return $lastId;
+        $req = $this->getDataBase()->prepare('SELECT LAST_INSERT_ID();');
+        $req->execute();
+        //recupe l'element 0 du array et transforme en int
+        return (int) $req->fetch()[0];
     }
+
+    // METHODE DE MODIFICATION D'UN POST
+    public function updated($postId, $title, $content)
+    {
+        $req = $this->getDataBase()->prepare('UPDATE posts SET title = ?, content = ?, creation_date = NOW() WHERE id = ? LIMIT 1');
+        return $req->execute(array($title, $content, $postId));
+    }   
+     
     // METHODE DE SUPPRESSION D'UN POST
     public function delete($postId)
     {   
-        var_dump('ok');
-        $req = $this->getDataBase()->prepare('DELETE FROM posts WHERE id= ?');
+        $req = $this->getDataBase()->prepare('DELETE FROM posts WHERE id = ?');
         return $req->execute(array($postId));
-        echo ('post supprimer');
     }
 }
