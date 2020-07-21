bea46226a7adbdd31557264f5c9218e5998596c0
Web82-Thom <thom.orta@gmail.com>
2020-07-21 15:20:33 +0200

    Ajout des commentaires sur la vue

--- view/displayPost.php
+++ view/displayPost.php
@@ -4,12 +4,25 @@
 <h2 id="titleListTicket"><?= htmlspecialchars($post->getTitle()); ?></h2>
 
 <div class="contentPosts">
-<h3 class="titleTicket"><?= htmlspecialchars($post->getTitle()); ?> publié le <?= $post->getCreation_date();?></h3>
-    <div id="post">
-        <p class="postText"><?= htmlspecialchars($post->getContent());?></p>
-        <p class="linkComment"><em><a class="link" href="../view/comment.php">Commentaires</a></em></p>
-    </div>  
-</div>    
+    <h3 class="titleTicket"><?= htmlspecialchars($post->getTitle()); ?> publié le <?= $post->getCreation_date();?></h3>
+        <div id="post">
+            <p class="postText"><?= htmlspecialchars($post->getContent());?></p>
+        
+        </div>
+    
+        
+</div>   
+<?php
+while ($comments = $comment->fetch())
+{ ?>
+    <div id="commentDisplay">
+        <p id="commentAuthor"><strong><?= htmlspecialchars($comments['author']) ?></strong> le <?= $comments['comment_date'] ?></p>
+        <p id="commentContent"><?= nl2br(htmlspecialchars($comments['comment'])) ?></p>
+    </div>
+<?php
+}
+?>
+       
 
 <?php $content = ob_get_clean(); ?>
 
