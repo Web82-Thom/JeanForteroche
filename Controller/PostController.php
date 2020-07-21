4f4dfc57be9a866aa11250b1abaf36f325404f65
Web82-Thom <thom.orta@gmail.com>
2020-07-21 15:09:16 +0200

    amelioration de la modification et suppression / ajout des commentaires

--- Controller/PostController.php
+++ Controller/PostController.php
@@ -3,26 +3,30 @@
 namespace Controller;
 
 use Model\PostManager;
+use Model\CommentManager;
 
 class PostController 
 {
     private $_postManager;
-    
+    private $_commentManager;
+
+    public function __construct()
+    {
+        $this->_postManager = new PostManager();
+        $this->_commentManager = new CommentManager();
+    }
+
     // RECUPERATION D'UN POST
     public function display($postId)
     {
-        $this->_postManager = new PostManager();
-
         $post = $this->_postManager->getPost($postId);
-
+        $comment = $this->_commentManager->getComments($postId);
         require_once('../view/displayPost.php');
     }
 
     // RECUPERATION DE TOUS LES POSTS
     public function displayPosts()
     {   
-        $this->_postManager = new PostManager();
-
         $posts = $this->_postManager->getPosts();
 
         require_once('../view/displayPosts.php');
@@ -32,7 +36,6 @@ class PostController
     public function add()
     {   
         if (!empty($_POST['title']) && !empty($_POST['content'])) {
-            $this->_postManager = new PostManager();
             $postId = $this->_postManager->add($_POST['title'], $_POST['content']);
             //si postId est un nombre alors redirection sur l'affichage du post ajouter
             if ($postId > 0) {
@@ -44,58 +47,34 @@ class PostController
         } 
     }
 
-    // AFFICAHGE AVANT MODIFICATION UN POST
+    // AFFICHAGE et MODIFICATION
     public function update($postId) 
     {
-        $this->_postManager = new PostManager();
-        $post = $this->_postManager->getPost($postId);
-        require_once('../view/formUpdatePost.php');
-    }
-
-    // MODIFICATION UN POST
-    public function updated($postId, $title, $content) 
-    {
-        $this->_postManager = new PostManager();
-
-        $post = $this->_postManager->updated($postId, $title, $content);
-
-        $this->_postManager = new PostManager();
-
-        $post = $this->_postManager->getPost($postId);
-
-        require_once('../view/displayPost.php');
-    }
-
-    // AFFICHAGE AVANT SUPPRESSION D'UN POST
-    public function delete($postId)
-    {
-        $this->_postManager = new PostManager();
-        $post = $this->_postManager->getPost($postId);
-        require_once('../view/deletePost.php');
-    }
-
-    // SUPPRESSION
-    public function deleted($postId)
-    {
-        $this->_postManager = new PostManager();
-        $post = $this->_postManager->delete($postId);
-        header('Location: index.php?objet=admin');
-    
-    }
-     /*$this->_postManager = new PostManager();
-        traitement du formulaire
+        // traitement du formulaire
         if (!empty($_POST['title']) && !empty($_POST['content'])) {
-           $post = $this->_postManager->updated($postId, $title, $content);
-            //si postId est un nombre alors redirection sur l'affichage du post modifier
-            if ($postId >= 0) {
-                require_once('../view/admin.php');
-            }
-            throw new Exception('Impossible de modifier le post !');
+            $this->_postManager->update($postId, $_POST['title'], $_POST['content']);
+
+            header('Location: index.php?objet=post&id=' . $postId);
         // affichage du formulaire modification
         } else {
-            var_dump('else');
             $post = $this->_postManager->getPost($postId);
-        require_once('../view/formUpdatePost.php');;
-        } */
+            require_once('../view/formUpdatePost.php');;
+        }
+    }
+
+    // AFFICHAGE et SUPPRESSION
+    public function delete($postId)
+    {
+        //traitement du formulaire
+        if (!empty($_POST['title']) && !empty($_POST['content'])) {
+            $this->_postManager->delete($postId);
+
+            header('Location: index.php?objet=admin');
+        // affichage du formulaire modification
+        } else {
+            $post = $this->_postManager->getPost($postId);
+            require_once('../view/formDeletePost.php');;
+        }
+    }
 }            
   
