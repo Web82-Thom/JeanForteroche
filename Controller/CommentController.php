ac9fa9048d4562edec26d1dd41bb86d6de719594
Web82-Thom <thom.orta@gmail.com>
2020-07-28 15:29:10 +0200

    fonction ajout et suppression d'un commentaire

--- Controller/CommentController.php
+++ Controller/CommentController.php
@@ -7,34 +7,44 @@ use Model\CommentManager;
 
 class CommentController 
 {
-    private $_postManager;
     private $_commentManager;
 
     public function __construct()
     {
-        $this->_postManager = new PostManager();
         $this->_commentManager = new CommentManager();
     }
 
-    // AFFICHAGE D'UN COMMENTAIRE
-    public function getComments($postId)
-    {
-        $getComment = $this->_commentManager->getComments($postId);
-        header('Location: index.php?objet=post&id=' . $postId);
-    }
-
     // AJOUT D'UN COMMENTAIRE
-    public function addComment($postId)
-    {
+    public function add($postId)
+    {   
+        //traitement du formulaire
         if (!empty($_POST['author']) && !empty($_POST['comment'])) {
-            $addComment = $this->_commentManager->addComment($postId, $author, $comment);
-            //si postId est un nombre alors redirection sur l'affichage du post ajouter
+            $add = $this->_commentManager->add($postId, $author, $comment);
             if ($postId > 0) {
                 header('Location: index.php?objet=post&id=' . $postId);
             }
+
             throw new Exception('Impossible d\'ajouter le commentaire !');
-        } else {
-            header('Location: index.php?objet=post&id=' . $postId);
         }
+            
+        header('Location: index.php?objet=post&id=' . $postId);
+    }
+
+    // SUPRESSION D'UN COMMENTAIRE
+    public function delete($commentId)
+    {
+        //traitement du formulaire
+        if (!empty($_POST['author']) && !empty($_POST['comment'])) {
+            $this->_commentManager->delete($commentId);
+            if ($commentId > 0) {
+                header('Location: index.php?objet=admin');
+            }
+
+            throw new Exception('Impossible de supprimer le commentaire !');
+        
+        } 
+        // affichage du formulaire pour la suppression
+        $comment= $this->_commentManager->getComment($commentId);
+        require_once('../view/formDeleteComment.php');
     }
 }
