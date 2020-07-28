6efdd5481e38110eca80b6e927be58cabd128ac8
Web82-Thom <thom.orta@gmail.com>
2020-07-28 09:39:41 +0200

    formating

--- view/displayPosts.php
+++ view/displayPosts.php
@@ -1,17 +1,20 @@
 <?php $title = "Billets à lire"; ?>
 
 <?php ob_start(); ?>
+
 <h2 id="titleListTicket">liste des Billets</h2>
 <?php foreach ($posts as $post) { ?>
-<div class="contentPosts">
-<h3 class="titleTicket"><a href="index.php?objet=post&amp;id=<?= $post->getId(); ?>"><?= htmlspecialchars($post->getTitle());?></a> publié le <?= $post->getCreation_date();?></h3>
-    <div id="post">
-        <p class="postText"><?= $post->getContent();?></p>
-        <p class="linkComment"><em><a class="link" href="index.php?objet=post&amp;id=<?= $post->getId(); ?>">Commentaires</a></em></p>
-    </div>  
-</div>    
-
+    <div class="contentPosts">
+        <h3 class="titleTicket"><a href="index.php?objet=post&amp;id=<?= $post->getId(); ?>"><?= htmlspecialchars($post->getTitle());?></a> publié le <?= $post->getCreation_date();?></h3>
+        <div id="post">
+            <p class="postText"><?= $post->getContent();?></p>
+            <p class="linkComment"><em><a class="link" href="index.php?objet=post&amp;id=<?= $post->getId(); ?>">Commentaires</a></em></p>
+        </div>  
+    </div>    
 <?php } ?>
-<?php $content = ob_get_clean(); ?>
 
-<?php require_once('template.php');
+<?php 
+$content = ob_get_clean(); 
+
+require_once('template.php');
+ ?>
