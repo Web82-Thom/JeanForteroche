2477d4b146dba06f96253c6a8da9bfcb1a66700d
Thomas <lefoux82@gmail.com>
2020-08-11 10:33:21 +0200

    ajout des liens connection et deconnection

--- view/menu.php
+++ view/menu.php
@@ -1,8 +1,16 @@
 <h1>Jean Forteroche</h1>
-<h2>"Billet simple pour l'Alaska"</h2>
-    <ul id="menuNavigation">
-        <li> <a href="index.php?objet=home">Présentation</a></li>
-        <li> <a href="index.php?objet=post">Liste des Billets</a></li>
-        <li> <a href="#">Contact</a></li>
-        <li> <a href="index.php?objet=admin">Admin</a></li>
-    </ul>
+
+<div>
+    <h2>"Billet simple pour l'Alaska"</h2>
+        <ul id="menuNavigation">
+            <li> <a href="index.php?objet=home">Présentation</a></li>
+            <li> <a href="index.php?objet=post">Liste des Billets</a></li>
+            <li> <a href="#">Contact</a></li>
+            <li id="menuAdmin"> <a href="">Admin</a>
+                <ul id="sousMenu">
+                    <li><a href="index.php?objet=admin&action=login">Se connecter</a></li>
+                    <li><a href="index.php?objet=admin&action=destroy">Se déconnecter</a></li>
+                </ul>
+            </li>
+        </ul>
+</div>
