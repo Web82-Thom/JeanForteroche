b348ac410bf52ccb3d3f451a11238131024714b4
Thomas <lefoux82@gmail.com>
2020-08-12 20:28:15 +0200

    probleme ordre du foreach

--- view/formDeleteComment.php
+++ view/formDeleteComment.php
@@ -1,23 +1,24 @@
 <?php $title = "Suppression des Commentaires"; ?>
 
 <?php ob_start(); ?>
-
-<div id="formDeleteComment">
-    <h3 id="formTitle">Suppression de commentaire</h3>
-    <form id="formContent" method="POST" action="index.php?objet=post&action=deleteComment&id=<?= $comment->getId(); ?>">
-        <div>
-            <label for="author">Auteur</label><br />
-            <input type="text" id="author" name="author" <?php echo 'value="' . $comment->getAuthor() . '"'; ?>/>
-        </div>
-        <div>
-            <label for="comment">Commentaire</label><br />
-            <textarea id="comment" name="comment"><?= $comment->getComment(); ?></textarea>
-        </div>
-        <div>
-            <input type="submit" value="Supprimer le commentaire"/>
-        </div>
-    </form>
-</div>
+<?php foreach ($comment as $comments); { ?>
+    <div id="formDeleteComment">
+        <h3 id="formTitle">Suppression de commentaire</h3>
+        <form id="formContent" method="POST" action="index.php?objet=post&action=deleteComment&id=<?= $comments->getId(); ?>">
+            <div>
+                <label for="author">Auteur</label><br />
+                <input type="text" id="author" name="author" <?php echo 'value="' . $comments->getAuthor() . '"'; ?>/>
+            </div>
+            <div>
+                <label for="comment">Commentaire</label><br />
+                <textarea id="comment" name="comment"><?= $comments->getComment(); ?></textarea>
+            </div>
+            <div>
+                <input type="submit" value="Supprimer le commentaire"/>
+            </div>
+        </form>
+    </div>
+<?php } ?>
 
 <?php $content = ob_get_clean(); ?>
 
