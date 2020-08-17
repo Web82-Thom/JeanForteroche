aec95e155af1593d60cb316c45a306881e44d201
Thomas <lefoux82@gmail.com>
2020-08-17 08:28:22 +0200

    htmlspecialchars et script TinyMce

--- view/formAddPost.php
+++ view/formAddPost.php
@@ -6,11 +6,19 @@
     <div id ="form">
         <form method="post" action="index.php?objet=post&action=add">
             <p><label for="textarea" >Rédaction :</label></p>
-            <p><input type="text" placeholder="Titre" name ="title" <?php if (isset($post)) {echo 'value="' . $post->getTitle(). '"';} ?>></p>
-            <p><textarea id="mytextarea" placeholder ="Contenu" name ="content"><?php if (isset($post)) {echo $post->getContent();} ?></textarea></p>
+            <p><input type="text" placeholder="Titre" name ="title" <?php if (isset($post)) {echo 'value="' . htmlspecialchars($post->getTitle()). '"';} ?>></p>
+            <p><textarea id="mytextarea" placeholder ="Contenu" name ="content"><?php if (isset($post)) {echo htmlspecialchars($post->getContent());} ?></textarea></p>
             <p><input type="submit" value ="Envoyer un nouveau post!"></p>
         </form>
     </div>
+    
+  <script>
+    /*tinymce.init({
+      selector: 'textarea',
+      plugins: 'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+      toolbar_mode: 'floating',
+    });*/
+  </script>
 
 <?php $content = ob_get_clean(); ?>
 
