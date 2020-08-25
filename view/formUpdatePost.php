c0b35f591e4aced16dc0faa846898a3f06b0242a
Thomas <lefoux82@gmail.com>
2020-08-25 23:58:11 +0200

    modification du formulaire ajout du tinyMce

--- view/formUpdatePost.php
+++ view/formUpdatePost.php
@@ -1,19 +1,28 @@
 <?php $title = "Admin-Modification"; ?>
 
 <?php ob_start(); ?>
-
-<h3>TinyMCE traitement des Chapitres et envoie du fichier</h3>
-    <div id ="form">
+<div id ="form">
         <form method="post" action="index.php?objet=post&action=update&id=<?= $post->getId();?>">
-            <p>Modification :</p>
-            <p>
-                <label for="title">Titre</label>
-                <input type="text" placeholder="Titre" id="title" name="title" <?php if (isset($post)) {echo 'value="' . $post->getTitle(). '"';} ?>>
-            </p>
-            <p><textarea id="mytextarea" placeholder="Contenu" name="content"><?php if (isset($post)) {echo $post->getContent();} ?></textarea></p>
-            <p><input type="submit" value="Poster les modification"></p>
+            <h2>Modification du chapîtres</h2>
+            <div class="labelTitle">
+                <h3><label for="title">Titre :</label></h3>
+                <p><input type="text" placeholder="Titre" name ="title" <?php if (isset($post)) {echo 'value="' . $post->getTitle(). '"';} ?>></p>
+            </div>
+            <div class="labelContent">
+                <h3><label for="content">Contenu :</label></h3>
+                <p><textarea id="mytextarea" placeholder ="Rédiger votre chapître" name ="content"><?php if (isset($post)) {echo $post->getContent();} ?></textarea></p>
+            </div>
+                <button class="formButton" type="submit" value ="Poster les modification">Poster les modification</button>
         </form>
     </div>
+
+    <script>
+    tinymce.init({
+      selector: '#mytextarea',
+      plugins: 'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+      toolbar_mode: 'floating',
+    });
+  </script>
 
 <?php $content = ob_get_clean(); ?>
 
