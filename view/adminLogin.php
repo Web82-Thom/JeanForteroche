0793d6ccd813161a2a5dbca770b9302c0b724649
Thomas <lefoux82@gmail.com>
2020-08-25 10:15:23 +0200

    formating

--- view/adminLogin.php
+++ view/adminLogin.php
@@ -1,8 +1,8 @@
 <?php $title = "Login"; ?>
 
 <?php ob_start(); ?>
+
 <div id="formConnection">
-    
     <form id="formConnectionContent"method="post" action="index.php?objet=admin&action=login">
     <p>Veuillez remplir les champs avec vos identifiants de connection.</p>
         <label for="em">Renseignez votre email : </label>
