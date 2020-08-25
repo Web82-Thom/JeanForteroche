2dcfb2746c853bba9a624511ceb1d00f7bcef0d3
Thomas <lefoux82@gmail.com>
2020-08-25 10:08:15 +0200

    ajout du host commenter pour mise en ligne

--- Model/Database.php
+++ Model/Database.php
@@ -12,6 +12,7 @@ abstract class Database
     private static function setDataBase()
     {
         self::$_dataBase = new PDO('mysql:host=localhost;dbname=mydbase;charset=utf8', 'root', '');
+        //self::$_dataBase = new PDO('mysql:host=db810297898.hosting-data.io;dbname=db810297898;charset=utf8', 'dbo810297898', 'Miaimeaou.382');
         self::$_dataBase->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
     }
 
