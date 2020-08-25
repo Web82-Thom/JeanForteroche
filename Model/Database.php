584b6978f790b26b7093b57185b140bdc53e3de3
Thomas <lefoux82@gmail.com>
2020-08-25 12:15:12 +0200

    suppression des données de connections

--- Model/Database.php
+++ Model/Database.php
@@ -8,11 +8,13 @@ use PDO;
 abstract class Database
 {
     private static $_dataBase;
+
     //CONNEXION A LA BASE DE DONNEE
     private static function setDataBase()
     {
-        self::$_dataBase = new PDO('mysql:host=localhost;dbname=mydbase;charset=utf8', 'root', '');
-        //self::$_dataBase = new PDO('mysql:host=db810297898.hosting-data.io;dbname=db810297898;charset=utf8', 'dbo810297898', 'Miaimeaou.382');
+        require_once('../config/database.php');
+
+        //self::$_dataBase = $_dataBase;
         self::$_dataBase->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
     }
 
