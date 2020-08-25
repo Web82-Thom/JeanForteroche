b5c08b52b377b238e160ae938ac91b6ea0865934
Thomas <lefoux82@gmail.com>
2020-08-25 10:19:59 +0200

    modification du controle par $_SESSION['firstAdmin']

--- view/displayPost.php
+++ view/displayPost.php
@@ -39,7 +39,7 @@
         <em class="dateInfos">publié le <?= $comment->getCommentDate(); ?></em>
         <p id="commentContent"><?= htmlspecialchars($comment->getComment()); ?></p>
         <p><a class="adminLink" href="index.php?objet=post&action=reportComment&id=<?= $comment->getId() ; ?>&postId= <?= $post->getId(); ?>" onclick="return(confirm('Etes-vous sûr de vouloir signaler ce commentaire ?'));" onclick="window.location.reload(false)" ><?php if ($comment->getReport() == 1) { echo ''; } else { echo 'Signaler';} ?></a></p>
-        <p><a class="adminLink" href="index.php?objet=post&action=updateComment&id=<?= $comment->getId(); ?>&postId=<?= $comment->getPostId(); ?>"><?php if (isset($_SESSION['pseudo'])) { echo 'Modifier'; } else { echo '';} ?></a></p><br />
+        <p><a class="adminLink" href="index.php?objet=post&action=updateComment&id=<?= $comment->getId(); ?>&postId=<?= $comment->getPostId(); ?>"><?php if (isset($_SESSION['firstAdmin']) && $_SESSION['firstAdmin'] == 1) { echo 'Modifier'; } else { echo '';} ?></a></p>
         <p id="reportSignal"><?php if ($comment->getReport() == 1) { echo 'Commentaire signalé'; } ?> </p>     
     </div>
 <?php } ?>
