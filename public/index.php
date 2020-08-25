6b0de6fa97821ef619efe7d9fe91365aa777ce32
Thomas <lefoux82@gmail.com>
2020-08-25 12:17:08 +0200

    formating

--- public/index.php
+++ public/index.php
@@ -1,15 +1,16 @@
 <?php
-session_start();
-
 use Controller\Router;
 use Exception;
+
+session_start();
+
 //CHARGEMENT AUTO DES CLASSES RECONSTRUCTION DU CHEMIN
 spl_autoload_register(function($class) {
     $class = '../' . str_replace("\\", '/', $class) . '.php';
     if(file_exists($class)) {
         require_once($class);
     } else { 
-        throw new \Exception($class);
+        throw new Exception($class);
     }
 });
 
