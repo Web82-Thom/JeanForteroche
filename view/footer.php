3e9d017a033f4809a0643ed4cb1f41b6b6b523c8
Thomas <lefoux82@gmail.com>
2020-08-26 03:02:28 +0200

    Formating du menuFooter

--- view/footer.php
+++ view/footer.php
@@ -1,9 +1,15 @@
 <!--FOOTER-->
 <footer>
-    <ul id="menuFooter">
-        <li> <a href="index.php?objet=home"> Présentation</a></li>
-        <li> <a href="index.php?objet=post">Liste des Billets</a></li>
-        <li> <a href="#">Contact</a></li>
-        <li> <a href="index.php?objet=admin">Admin</a></li>
-    </ul>
+	<ul id="menuFooter">
+		<li><a href="index.php?objet=home">Présentation</a></li>
+		<li><a href="index.php?objet=post">Liste des Billets</a></li>
+		<li><a href="index.php?objet=contact">Contact</a></li>
+		<li id="menuAdmin"><a href="index.php?objet=admin&action=login">Admin</a>
+			<ul id="sousMenu">
+				<li><a href="index.php?objet=admin&action=login">Se connecter</a></li>
+				<li><hr></li>
+				<li><a href="index.php?objet=admin&action=destroy">Se déconnecter</a></li>
+			</ul>
+		</li>
+	</ul>
 </footer>
