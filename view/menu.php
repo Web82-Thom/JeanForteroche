0cf08005b0bce9c0a3b0c4e1f852c0ddbf98f978
Thomas <lefoux82@gmail.com>
2020-08-26 02:52:08 +0200

    Ajout de id='titleBook' et <hr>

--- view/menu.php
+++ view/menu.php
@@ -1,6 +1,6 @@
 <h1>Jean Forteroche</h1>
 <div>
-    <h2>"Billet simple pour l'Alaska"</h2>
+    <h2 id="titleBook">"Billet simple pour l'Alaska"</h2>
         <ul id="menuNavigation">
             <li> <a href="index.php?objet=home">Présentation</a></li>
             <li> <a href="index.php?objet=post">Liste des Billets</a></li>
@@ -8,6 +8,7 @@
             <li id="menuAdmin"> <a href="index.php?objet=admin&action=login">Admin</a>
                 <ul id="sousMenu">
                     <li><a href="index.php?objet=admin&action=login">Se connecter</a></li>
+                    <li><hr></li>
                     <li><a href="index.php?objet=admin&action=destroy">Se déconnecter</a></li>
                 </ul>
             </li>
