de7ff95561d3bb354b15d476134ecf3c98b6e1e8
Thomas <lefoux82@gmail.com>
2020-08-29 08:40:10 +0200

    insertion des liens script pour le controle du formulaire de contact

--- view/formContact.php
+++ view/formContact.php
@@ -10,7 +10,7 @@
         <input id="inputContactEmail" type="email" name="email" placeholder="Votre Email" size="30" maxlength="50"><br>
         <input id="inputContactTitle" type="text" name="title" placeholder="Titre" size="30"><br>
         <textarea id="inputContactMessage" type="text" name="message" rows="10" cols="30" placeholder="Votre message" maxlength="150"></textarea><br>
-        <button id="buttonSend" type="submit" name="mailForm" title="Envoyer"> Envoyer votre message</button>
+        <button id="buttonSend" type="submit" name="mailForm" title="Envoyer" >Envoyer votre message</button>
         <p id="echoMessageContact"><?php if (isset($message)) { echo $msg; } ?></p>
     </form>
 </div>
@@ -20,3 +20,6 @@ $content = ob_get_clean();
 
 require_once('template.php'); 
 ?>
+
+<script src="js/formContact.js"></script>
+<script src="js/main.js"></script>
