1266d0c899fbd379663fa82413ec3081051a1877
Thomas <lefoux82@gmail.com>
2020-09-01 11:42:59 +0200

    identation, changement id de eventListener, condition de verification du formulaire de contact

--- public/js/formContact.js
+++ public/js/formContact.js
@@ -2,22 +2,14 @@ class FormContact {
     constructor() {
         this.eventListenerMethode();
 
-        this.form = document.getElementById('formContact');
-        
         document.getElementById('inputContactName').value = localStorage.getItem('name');
-
         document.getElementById('inputContactFirstName').value = localStorage.getItem('firstName');
-        
-        document.getElementById('inputContactEmail').value = sessionStorage.getItem('Email');
-
-        document.getElementById('inputContactTitle').value = localStorage.getItem('Titre');
-
+        document.getElementById('inputContactEmail').value = localStorage.getItem('email');
         document.getElementById('inputContactMessage').value = localStorage.getItem('message');
-
     }
 
     eventListenerMethode() {
-        document.getElementById('buttonSend').addEventListener('click' , () => this.verification());
+        document.getElementById('formButton').addEventListener('click' , () => this.verification());
     }
 
     verification() {
@@ -27,12 +19,17 @@ class FormContact {
         let title = document.getElementById('inputContactTitle').value;
         let message = document.getElementById('inputContactMessage').value;
 
-        if (name !=="" && firstName !=="" && email !=="" && title !=="" &&message  !=="" ) {
-            sessionStorage.setItem('email', email);
+        if (name !== "" &&
+            firstName !== "" && 
+            email !== "" && 
+            title !== "" &&
+            message  !== "" ) {
+            localStorage.setItem('email', email);
+            localStorage.setItem('name', name);
+            localStorage.setItem('firstName', firstName);
 
-            document.getElementById("formContent").submit();
             alert('Votre message à été envoyer avec succes!');
-            
+            document.getElementById("formContent").submit();
         } else {
             alert('Veuillez remplir tous les champs correctement.');
         }
