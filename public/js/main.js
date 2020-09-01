fc67a0e72cc4790063d0be83c9f8b920c735da9c
Thomas <lefoux82@gmail.com>
2020-09-01 11:41:49 +0200

    condition du main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,9 @@
-new FormContact();
+
+if (document.getElementById('formContact')) {
+    new FormContact();
+} else if (document.getElementById('formConnection')) {
+    new FormConnection();
+} else if (document.getElementById('formAddComment')) {
+    new FormAddComment();
+}
+
