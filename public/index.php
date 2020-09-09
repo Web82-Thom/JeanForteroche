59a8c527269195f30bb308ed9e4729fefbb2e54d
Thomas <lefoux82@gmail.com>
2020-09-09 10:28:40 +0200

    Nettoyage

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ session_start();
 //CHARGEMENT AUTO DES CLASSES RECONSTRUCTION DU CHEMIN
 spl_autoload_register(function($class) {
     $class = '../' . str_replace("\\", '/', $class) . '.php';
-    if(file_exists($class)) {
+    if (file_exists($class)) {
         require_once($class);
     } else { 
         throw new Exception($class);
